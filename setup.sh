8b8e1d390a98a83ff7711578177811e9421c667f
Ying Huang <huangyingw@gmail.com>
2020-01-26 21:33:39 -0800

    n

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ apt install curl
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt install -y kubeadm && \
     swapoff -a && \
-    hostnamectl set-hostname master-node && \
     kubeadm init --pod-network-cidr=10.244.0.0/16
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
