756ea95917979a908057137b660f9a2c0c09dc1a
Ying Huang <huangyingw@gmail.com>
2020-01-26 21:49:51 -0800

    n

--- reset.sh
+++ reset.sh
@@ -3,4 +3,6 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+rm -fr ~/.kube/
 kubeadm init --pod-network-cidr=10.244.0.0/16
+kubectl taint nodes --all node-role.kubernetes.io/master-
