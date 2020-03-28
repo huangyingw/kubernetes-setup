e4dc15350a6ab4cc4bd08ee69253de9454ba17cf
Ying Huang <huangyingw@gmail.com>
2020-03-27 21:54:31 -0700

    n

--- setup.sh
+++ setup.sh
@@ -10,4 +10,9 @@ apt install -y kubeadm && \
     swapoff -a && \
     kubeadm init --pod-network-cidr=10.244.0.0/16
 
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
 kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
