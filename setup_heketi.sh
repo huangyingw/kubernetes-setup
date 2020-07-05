2fa5dd7872527218edfc1b96f149070098cb12e5
Ying Huang <huangyingw@gmail.com>
2020-07-05 11:57:28 -0400

    n

--- setup_heketi.sh
+++ setup_heketi.sh
@@ -17,3 +17,20 @@ groupadd --system heketi
 useradd -s /sbin/nologin --system -g heketi heketi
 mkdir -p /var/lib/heketi /etc/heketi /var/log/heketi
 cp heketi/heketi.json /etc/heketi
+for i in dm_snapshot dm_mirror dm_thin_pool; do
+  modprobe $i
+done
+ssh-keygen -f /etc/heketi/heketi_key -t rsa -N ''
+chown heketi:heketi /etc/heketi/heketi_key*
+
+for i in 10.1.10.95; do
+  ssh-copy-id -i /etc/heketi/heketi_key.pub root@$i
+done
+/usr/bin/ssh -i /etc/heketi/heketi_key root@10.1.10.95
+cp -v heketi.service /etc/systemd/system/heketi.service
+wget -O -nc /etc/heketi/heketi.env https://raw.githubusercontent.com/heketi/heketi/master/extras/systemd/heketi.env
+chown -R heketi:heketi /var/lib/heketi /var/log/heketi /etc/heketi
+ufw disable
+systemctl daemon-reload
+systemctl enable --now heketi
+systemctl status heketi
