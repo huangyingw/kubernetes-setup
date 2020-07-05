09d638ac2126ecee38f950024d133addfe7e8831
Ying Huang <huangyingw@gmail.com>
2020-07-05 19:49:13 -0400

    n

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 apt install curl
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt install -y kubeadm && \
