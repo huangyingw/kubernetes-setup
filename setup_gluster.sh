cb140dd2c36522ad5c408cea7b7c729a2984ae1a
Ying Huang <huangyingw@gmail.com>
2020-07-04 23:52:18 -0400

    n

--- setup_gluster.sh
+++ setup_gluster.sh
@@ -7,11 +7,9 @@ https://docs.gluster.org/en/latest/Install-Guide/Install/
 apt install software-properties-common
 add-apt-repository ppa:gluster/glusterfs-7
 apt update
-apt install glusterfs-server
+apt install -y glusterfs-server glusterfs-client
 
 mkdir -p /media/nvme/glusterfs/distributed
-gluster volume create vol01 transport tcp 10.1.10.95:/media/nvme/glusterfs/distributed force
-gluster volume start vol01
-gluster volume info vol01
-
-../examples/volumes/glusterfs/README.md
+gluster volume create kube_vol transport tcp 10.1.10.95:/media/nvme/glusterfs/distributed force
+gluster volume start kube_vol
+gluster volume info kube_vol
