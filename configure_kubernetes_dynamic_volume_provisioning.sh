d81f4cd8d088c21a793606c0f36a703a8f607824
Ying Huang <huangyingw@gmail.com>
2020-07-06 12:03:57 -0400

    n

--- configure_kubernetes_dynamic_volume_provisioning.sh
+++ configure_kubernetes_dynamic_volume_provisioning.sh
@@ -9,3 +9,5 @@ echo -n "PASSWORD" | base64
 kubectl create -f gluster-secret.yaml
 kubectl get secret
 kubectl create -f gluster-sc.yaml
+kubectl get sc
+kubectl create --save-config -f glusterfs-pvc.yaml
