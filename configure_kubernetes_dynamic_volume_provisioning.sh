896ecfd38c6055c081c14c65ceba953fb9cf3724
Ying Huang <huangyingw@gmail.com>
2020-07-06 12:46:51 -0400

    n

--- configure_kubernetes_dynamic_volume_provisioning.sh
+++ configure_kubernetes_dynamic_volume_provisioning.sh
@@ -8,7 +8,11 @@ echo -n "PASSWORD" | base64
 
 kubectl create -f gluster-secret.yaml
 kubectl get secret
+
+kubectl delete storageclasses.storage.k8s.io gluster-heketi 
 kubectl create -f gluster-sc.yaml
 kubectl get sc
+
+kubectl delete persistentvolumeclaims gluster-pvc 
 kubectl create --save-config -f glusterfs-pvc.yaml
 kubectl get  pvc
