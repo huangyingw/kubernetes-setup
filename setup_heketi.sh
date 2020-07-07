9a4b6fe36ae6f8c17973a8698cf1076812e6f4f3
Ying Huang <huangyingw@gmail.com>
2020-07-07 01:08:03 -0400

    n

--- setup_heketi.sh
+++ setup_heketi.sh
@@ -13,6 +13,9 @@ curl -s https://api.github.com/repos/heketi/heketi/releases/latest \
 
 for i in `ls | grep heketi | grep .tar.gz`; do tar xvf $i; done
 cp heketi/{heketi,heketi-cli} /usr/local/bin
+heketi --version
+heketi-cli --version
+
 groupadd --system heketi
 useradd -s /sbin/nologin --system -g heketi heketi
 mkdir -p /var/lib/heketi /etc/heketi /var/log/heketi
@@ -24,10 +27,10 @@ done
 ssh-keygen -f /etc/heketi/heketi_key -t rsa -N ''
 chown heketi:heketi /etc/heketi/heketi_key*
 
-for i in 10.1.10.177; do
+for i in gluster00 gluster01 gluster02; do
   ssh-copy-id -i /etc/heketi/heketi_key.pub root@$i
 done
-# /usr/bin/ssh -i /etc/heketi/heketi_key root@10.1.10.177
+# /usr/bin/ssh -i /etc/heketi/heketi_key root@gluster01
 cp -fv heketi.service /etc/systemd/system/heketi.service
 systemctl daemon-reload
 systemctl enable --now heketi
