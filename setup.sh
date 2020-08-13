33188763835d5aed0fb2045e4b683a59afd55377
Ying Huang <huangyingw@gmail.com>
2020-08-13 12:10:31 -0400

    n

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ apt install -y kubeadm && \
     swapoff -a && \
     kubeadm init --pod-network-cidr=10.244.0.0/16
 
+swapoff -a
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
