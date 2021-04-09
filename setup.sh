14a7dead2df50b382aa81dde031964a676e28cac
Ying Huang <huangyingw@gmail.com>
2021-04-08 21:51:04 -0700

    n

--- setup.sh
+++ setup.sh
@@ -3,14 +3,15 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+apt install -y curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
-apt install curl
 snap install kube-controller-manager
 
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt install -y kubeadm && \
-    swapoff -a && \
-    kubeadm init --pod-network-cidr=10.244.0.0/16
+apt install -y kubeadm
+swapoff -a
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
 
 swapoff -a
 mkdir -p $HOME/.kube
