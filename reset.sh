bf96ddeb0ffea02e84f6203257520c89f17cecdb
Ying Huang <huangyingw@gmail.com>
2021-04-08 23:02:10 -0700

    n

--- reset.sh
+++ reset.sh
@@ -4,13 +4,4 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 ./remove_kubernetes_config.sh
-kubeadm init --pod-network-cidr=10.244.0.0/16
-
-swapoff -a
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-
-kubectl taint nodes --all node-role.kubernetes.io/master-
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+./configure_kubernetes.sh
