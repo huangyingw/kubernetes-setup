b80c992e9cc1ac64b84b820eadf77e3e66bcefc5
Ying Huang <huangyingw@gmail.com>
2021-04-10 16:15:50 -0700

    n

---
+++ openebs_all.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+
+./prerequisites.sh
+./verify.sh
+./set_cluster_admin_user_context.sh
+kubectl apply -f ./k8s/openebs-operator.yaml
