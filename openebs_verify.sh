dab4ac4eab9770c69af18e8667427a421cc31876
Ying Huang <huangyingw@gmail.com>
2021-04-10 20:20:20 -0700

    n

--- openebs_verify.sh
+++ openebs_verify.sh
@@ -3,5 +3,8 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-cat /etc/iscsi/initiatorname.iscsi
-systemctl status iscsid
+kubectl get pods -n openebs
+kubectl get sc
+kubectl get blockdevice -n openebs
+# kubectl describe blockdevice blockdevice-c3f4da0efa6b0330fde5a02647a5f8df -n openebs
+kubectl get sp
