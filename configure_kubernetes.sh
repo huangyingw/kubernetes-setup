e6f199d2f4096912ae0e9e8ef01707f689f86338
Ying Huang <huangyingw@gmail.com>
2021-04-24 22:21:30 -0700

    n

--- configure_kubernetes.sh
+++ configure_kubernetes.sh
@@ -9,6 +9,6 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl taint nodes --all node-role.kubernetes.io/master-
+# kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
